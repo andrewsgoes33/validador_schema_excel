2e6a0b8b93038f3d02b8bb69cc1beb57287fc542
Andrews Goes <andrews.goes.33@gmail.com>
2024-08-06 08:49:17 -0300

    tests: refatorando nosso teste

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,20 +2,42 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.firefox.service import Service
 from time import sleep
+import pytest
+import subprocess
 
-# Precisamos definir qual driver vamos utilizar
-service = Service('C:/geckodriver/geckodriver.exe')
-driver = webdriver.Firefox(service=service)
+@pytest.fixture
 
-# Define um timeout implícito
-driver.set_page_load_timeout(5)  # 5 segundos
+def driver():
+    # Iniciar o Streamlit em background
+    process = subprocess.Popen(["streamlit", "run", "src/app.py"])
 
-# Vamos fazer uma tratativa de try-except de entrar na nossa página
-try:
+    # Iniciar o WebDriver usando GeckoDriver
+    driver = webdriver.Firefox()
+    driver.set_page_load_timeout(5)
+    yield driver
+
+    # Fechar o WebDriver e o Streamlit após o teste
+    driver.quit()
+    process.kill()
+
+def test_app_opens(driver):
+    # Verificar se a página abre
     driver.get("http://localhost:8501")
     sleep(5)
-    print("Acessou a página com sucesso")
-except TimeoutException:
-    print("Tempo de carregamento da página excedeu o limite.")
-finally:
-    driver.quit()
+
+# # Precisamos definir qual driver vamos utilizar
+# service = Service('C:/geckodriver/geckodriver.exe')
+# driver = webdriver.Firefox(service=service)
+
+# # Define um timeout implícito
+# driver.set_page_load_timeout(5)  # 5 segundos
+
+# # Vamos fazer uma tratativa de try-except de entrar na nossa página
+# try:
+#     driver.get("http://localhost:8501")
+#     sleep(5)
+#     print("Acessou a página com sucesso")
+# except TimeoutException:
+#     print("Tempo de carregamento da página excedeu o limite.")
+# finally:
+#     driver.quit()
